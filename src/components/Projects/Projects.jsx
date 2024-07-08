68430f640237cc29ced553ebcabeb128b6585027
Kanchan Duwal <knchnduwal@gmail.com>
2024-07-08 18:41:55 +0545

    ProjectCard

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,25 @@
 import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/viberr.png";
+import ProjectCard from "../common/ProjectCard";
 
 const Projects = () => {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
-      <div className={styles.projects}></div>
+      <div className={styles.projectsContainer}>
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/knchndwl/kanchan-resume"
+          h3='viberr'
+          p='Streaming App'
+        />
+        <ProjectCard
+          src={viberr}
+          link="https://github.com/knchndwl/kanchan-resume"
+          h3='viberr'
+          p='Streaming App'
+        />
+      </div>
     </section>
   );
 };
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import styles from './SkillsStylesModule.css'
+
+const Skills = () => {
+  return (
+<section>
+    <h1>Skills</h1>
+</section>
+  )
+}
+
+export default Skills
